ce0249741773651e419583c13904204db633c9bf
Aleks Redman <aleksredman@gmail.com>
2017-10-28 17:56:57 +0300

    add js slick initialization

---
+++ source/js/initSlider.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	'use strict';
+	$('.slider').slick({
+		accessibility: true,
+		adaptiveHeight: true,
+		arrows: false,
+		dots: false,
+		mobileFirst: true,
+		vertical: false,
+		verticalSwiping: false
+	});
+});
